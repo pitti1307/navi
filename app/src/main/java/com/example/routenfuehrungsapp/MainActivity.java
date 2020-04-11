159d127cf0d87f09087bbaf0097c045a13c33c5c
Tuan Son Dinh <tuansondinh96@gmail.com>
2020-04-11 20:43:10 +0200

    Connect to MySQL, Get data

--- app/src/main/java/com/example/routenfuehrungsapp/MainActivity.java
+++ app/src/main/java/com/example/routenfuehrungsapp/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.routenfuehrungsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+
+       downloadJSON("http://10.0.2.2:8080/api.php");
+       // downloadJSON("https://stackoverflow.com/questions/13775103/how-do-i-know-if-i-have-successfully-connected-to-the-url-i-opened-a-connection");
+    }
+
+    private void downloadJSON(final String urlWebService) {
+
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                   loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    System.out.println(con.getResponseCode());
+
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String line = bufferedReader.readLine();
+                    while (line != null){
+                        sb.append(line);
+                        line = bufferedReader.readLine();
+                    }
+
+
+                    System.out.println(sb.toString());
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        JSONObject data= jsonArray.getJSONObject(50);
+        String adress;
+        adress=data.getString("Adresse");
+        System.out.println(adress);
+        /*String[] stocks = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            stocks[i] = obj.getString("name") + " " + obj.getString("price");
+        }*/
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stocks);
+        //listView.setAdapter(arrayAdapter);
     }
 }
